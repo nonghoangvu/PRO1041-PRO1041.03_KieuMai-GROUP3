78668883a4b53796a28f6b23c59b0f48e3429f78
vunhph33506 <vunhph33506@fpt.edu.vn>
2024-01-14 13:42:15 +0700

    New Template

---
+++ PRO1041-PRO1041.03_KieuMai-GROUP3/src/main/java/com/haladesign/PRO1041PRO104103_KieuMaiGROUP3.java
@@ -0,0 +1,12 @@
+package com.haladesign;
+
+/**
+ *
+ * @author NONG HOANG VU
+ */
+public class PRO1041PRO104103_KieuMaiGROUP3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
