7366c7f418d84fb67f9328f6cc2f70c7a594a548
vunhph33506 <vunhph33506@fpt.edu.vn>
2024-01-14 13:48:27 +0700

    Test 001

--- PRO1041-PRO1041.03_KieuMai-GROUP3/src/main/java/com/haladesign/PRO1041PRO104103_KieuMaiGROUP3.java
+++ PRO1041-PRO1041.03_KieuMai-GROUP3/src/main/java/com/haladesign/PRO1041PRO104103_KieuMaiGROUP3.java
@@ -8,5 +8,6 @@ public class PRO1041PRO104103_KieuMaiGROUP3 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("San pham bo sung");
     }
 }
