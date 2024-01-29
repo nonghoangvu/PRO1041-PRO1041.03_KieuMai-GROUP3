04a8c468155639f78ebb6cb41bf89be39efa998a
vunhph33506 <vunhph33506@fpt.edu.vn>
2024-01-30 00:51:14 +0700

    Formart Data

--- PRO1041-PRO1041.03_KieuMai-GROUP3/src/main/java/haladesign/Test.java
+++ PRO1041-PRO1041.03_KieuMai-GROUP3/src/main/java/haladesign/Test.java
@@ -1,10 +1,5 @@
 package haladesign;
 
-import haladesign.Utitlity.BcryptHash;
-import haladesign.model.SanPham;
-import haladesign.repository.ISanPham;
-import haladesign.repository.ISize;
-import haladesign.service.SanPhamService;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
@@ -33,13 +28,8 @@ public class Test {
                 .run(args);
     }
 
+    
+
     public static void main(String[] args) {
-//        context = createApplicationContext(args);
-//        ISize s = getBean(ISize.class);
-//        s.findAll().forEach(sp -> {
-//            System.out.println(sp);
-//        });
-        BcryptHash b = new BcryptHash();
-        System.out.println(b.encodeBase64("Tài khoản của tôi"));
     }
 }
